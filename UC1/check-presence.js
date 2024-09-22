75a40bf0346e3f8a459e287536f99aacce9eab55
Anjali Gour <gour26000@gmail.com>
2024-09-22 18:15:24 +0530

    [FEAT] : Anjali - Added check for Employee presence (UC 1)

---
+++ UC1/check-presence.js
@@ -0,0 +1,10 @@
+// UC1: Check if employee is present or absent
+
+const isEmployeePresent = Math.floor(Math.random() * 2); // 0 or 1
+
+if (isEmployeePresent) {
+    console.log("Employee is present.");
+} 
+else {
+    console.log("Employee is absent.");
+}
